eef5f46e7ef5eaa096884fb04571877ca6dfb57c
EhsanParsania <75175231+EhsanParsania@users.noreply.github.com>
2022-01-10 18:20:33 +0330

    Show user data on redirect

--- frontend/steam-login/src/routes/route.js
+++ frontend/steam-login/src/routes/route.js
@@ -1,14 +1,11 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import { App } from '../App'
-import { Return } from '../Return'
 
 export function AppRoutes() {
   return (
     <Routes>
       <Route path="/" element={<App />} />
-      <Route path="/return" element={<Return />} />
-
     </Routes>
   )
 }
