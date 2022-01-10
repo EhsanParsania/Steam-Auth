c4bc5eb6f9e9452b35755245ae2966f9972b6c31
EhsanParsania <75175231+EhsanParsania@users.noreply.github.com>
2022-01-10 17:02:54 +0330

    Edit link url

--- frontend/steam-login/src/App.js
+++ frontend/steam-login/src/App.js
@@ -1,21 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         Login with Steam
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="http://localhost:7069/api/auth/steam"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+         Login with Steam
         </a>
       </header>
     </div>
