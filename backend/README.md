e1bcf5b7323655761e8f165391114f4fc9b9f1c4
EhsanParsania <75175231+EhsanParsania@users.noreply.github.com>
2022-01-10 16:23:51 +0330

    Init backend

---
+++ backend/README.md
@@ -0,0 +1,5 @@
+# Sign in through Steam using NodeJS
+
+Code for the medium post with the same title.
+
+If you have any questions, hit me up at kevin@jkmholding.com
---
+++ backend/app.js
@@ -0,0 +1,61 @@
+var express = require('express');
+var passport = require('passport');
+var session = require('express-session');
+var passportSteam = require('passport-steam');
+var SteamStrategy = passportSteam.Strategy;
+var app = express();
+
+var port = 3000;
+
+// Required to get data from user for sessions
+passport.serializeUser((user, done) => {
+	done(null, user);
+});
+
+passport.deserializeUser((user, done) => {
+	done(null, user);
+});
+
+// Initiate Strategy
+passport.use(new SteamStrategy({
+	returnURL: 'http://localhost:' + port + '/api/auth/steam/return',
+	realm: 'http://localhost:' + port + '/',
+	apiKey: 'YOUR_API_KEY'
+	}, function (identifier, profile, done) {
+		process.nextTick(function () {
+			profile.identifier = identifier;
+			return done(null, profile);
+		});
+	}
+));
+
+app.use(session({
+	secret: 'Whatever_You_Want',
+	saveUninitialized: true,
+	resave: false,
+	cookie: {
+		maxAge: 3600000
+	}
+}));
+
+app.use(passport.initialize());
+
+app.use(passport.session());
+
+// Initiate app
+app.listen(port, () => {
+	console.log('Listening, port ' + port);
+});
+
+app.get('/', (req, res) => {
+	res.send(req.user);
+});
+
+// Routes
+app.get('/api/auth/steam', passport.authenticate('steam', {failureRedirect: '/'}), function (req, res) {
+	res.redirect('/')
+});
+
+app.get('/api/auth/steam/return', passport.authenticate('steam', {failureRedirect: '/'}), function (req, res) {
+	res.redirect('/')
+});
